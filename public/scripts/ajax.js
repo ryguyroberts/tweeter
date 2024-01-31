44d77c9b93cfcac05b2c3f13cd6deb97c3c23fc0
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-31 08:45:21 -0800

    Add data validation to form, empty or less than 140 chars

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -2,20 +2,25 @@ $(document).ready(function() {
   // Get form
   const $form = $('#new-tweet');
     $form.submit(function(event) {
-      // No more refresh
-      event.preventDefault();
+      const inputTextField = $form.find('textarea#tweet-text')
+      //input empty or over 140
+      if (inputTextField.val() === "" || inputTextField.val().length > 140) {
+        event.preventDefault();
+        alert("Tweet cannot be empty, or over 140 Chars!")
+      } else {
+        // No more refresh
+        event.preventDefault();
+        const formData = $form.serialize();
 
-      // we serialize the data
-      const formData = $form.serialize();
-
-      $.post("/tweets/", formData, function() {
-        //function that runs after ajax post sent
-        // Clear form
-        const inputTextField = $form.find('textarea#tweet-text')
-        inputTextField.val("");
-        //Reset char to 0
-        const counterElement = $form.find('.counter')
-        counterElement.text(140);
-      });
+        $.post("/tweets/", formData, function() {
+          //function that runs after ajax post sent
+          // Clear form
+          const inputTextField = $form.find('textarea#tweet-text')
+          inputTextField.val("");
+          //Reset char to 0
+          const counterElement = $form.find('.counter')
+          counterElement.text(0);
+        });
+      }
     });
 });
